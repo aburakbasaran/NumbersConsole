3a2c3466f5e44c972b6112a0fcd49ae4a1b5b136
serkan <srk1984@hotmail.com>
2020-02-06 22:41:23 +0300

    Commit

---
+++ ConsoleDemo/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ConsoleDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter 2 İnteger: ");
+            var val = Console.ReadLine();
+
+        }
+    }
+}
